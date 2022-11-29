b52eb369ff31b27dccb8f3050ae8e4374912084a
Emilson Silva <emilsongyn17@gmail.com>
2022-11-29 18:03:33 -0300

    Criando a tela base e BottomNavigationBar

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -2,6 +2,7 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:quitanda_virtual/src/auth/components/custom_text_field.dart';
 import 'package:quitanda_virtual/src/auth/sign_up_screen.dart';
+import 'package:quitanda_virtual/src/base/base_screen.dart';
 import 'package:quitanda_virtual/src/config/custom_colors.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -98,7 +99,12 @@ class SignInScreen extends StatelessWidget {
                             borderRadius: BorderRadius.circular(18),
                           ),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context)
+                              .pushReplacement(MaterialPageRoute(builder: (c) {
+                            return const BaseScreen();
+                          }));
+                        },
                         child: const Text(
                           'Entrar',
                           style: TextStyle(
