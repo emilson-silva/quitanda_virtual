3ca8fd162795471d4e9679787af770d4de82c1b0
Emilson Silva <emilsongyn17@gmail.com>
2022-11-30 12:10:45 -0300

    Criando valor total e botão concluir pedido

--- lib/src/pages/base/base_screen.dart
+++ lib/src/pages/base/base_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quitanda_virtual/src/pages/cart/cart_tab.dart';
 import 'package:quitanda_virtual/src/pages/home/home_tab.dart';
 
 class BaseScreen extends StatefulWidget {
@@ -20,7 +21,7 @@ class _BaseScreenState extends State<BaseScreen> {
         controller: pageController,
         children: [
           const HomeTab(),
-          Container(color: Colors.red),
+          CartTab(),
           Container(color: Colors.blue),
           Container(color: Colors.yellow),
         ],
