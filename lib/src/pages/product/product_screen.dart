95c84fef8801067a8de61395de002268f0b8b243
Emilson Silva <emilsongyn17@gmail.com>
2022-11-30 08:36:17 -0300

    Criação do widget de quantidade

--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quitanda_virtual/src/config/custom_colors.dart';
 import 'package:quitanda_virtual/src/models/item_model.dart';
+import 'package:quitanda_virtual/src/pages/commom_widgets/quantity_widget.dart';
 import 'package:quitanda_virtual/src/pages/home/components/utils_services.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -57,11 +58,7 @@ class ProductScreen extends StatelessWidget {
                               ),
                             ),
                           ),
-                          Container(
-                            height: 30,
-                            width: 70,
-                            color: Colors.red,
-                          ),
+                          const QuantityWidget()
                         ],
                       ),
                       Text(
