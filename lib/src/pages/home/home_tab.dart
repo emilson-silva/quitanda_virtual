c1f11f20aceae85767dc5debc70096ce98024afe
Emilson Silva <emilsongyn17@gmail.com>
2022-11-29 23:04:54 -0300

    Criação do grid para apresentação dos produtos

--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:quitanda_virtual/src/config/custom_colors.dart';
 import 'package:quitanda_virtual/src/pages/home/components/category_tile.dart';
 import 'package:quitanda_virtual/src/pages/home/components/search_field.dart';
+import 'package:quitanda_virtual/src/config/app_data.dart' as appData;
 
 class HomeTab extends StatefulWidget {
-  HomeTab({super.key});
+  const HomeTab({super.key});
 
   @override
   State<HomeTab> createState() => _HomeTabState();
@@ -78,18 +79,41 @@ class _HomeTabState extends State<HomeTab> {
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (_, index) {
                   return CategoryTile(
-                    category: categories[index],
-                    isSelected: categories[index] == selectedCategory,
+                    category: appData.categories[index],
+                    isSelected: appData.categories[index] == selectedCategory,
                     onPressed: () {
                       setState(() {
-                        selectedCategory = categories[index];
+                        selectedCategory = appData.categories[index];
                       });
                     },
                   );
                 },
                 separatorBuilder: (_, index) => const SizedBox(width: 10),
-                itemCount: categories.length),
-          )
+                itemCount: appData.categories.length),
+          ),
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.fromLTRB(
+                16,
+                0,
+                16,
+                16,
+              ),
+              physics: const BouncingScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                childAspectRatio: 9 / 11.5,
+              ),
+              itemCount: appData.items.length,
+              itemBuilder: (_, index) {
+                return Container(
+                  color: Colors.red,
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
