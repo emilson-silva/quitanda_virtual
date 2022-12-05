eb6a710a52054400715da7c5d1086744ca2ca502
Emilson Silva <emilsongyn17@gmail.com>
2022-12-05 11:26:51 -0300

    Adicionando ícone customizado na aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'QuitandaGoiana',
       theme: ThemeData(
         primarySwatch: Colors.green,
         scaffoldBackgroundColor: Colors.white.withAlpha(190),
