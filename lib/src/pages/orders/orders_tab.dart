cba4cd311bf8729c9d66a695e2e2b9cd8307bb14
Emilson Silva <emilsongyn17@gmail.com>
2022-12-06 12:18:29 -0300

    Ajustes

--- lib/src/pages/orders/orders_tab.dart
+++ lib/src/pages/orders/orders_tab.dart
@@ -3,7 +3,7 @@ import 'package:quitanda_virtual/src/config/app_data.dart' as appData;
 import 'package:quitanda_virtual/src/pages/orders/componets/order_tile.dart';
 
 class OrdersTab extends StatelessWidget {
-  const OrdersTab({super.key});
+  const OrdersTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +12,10 @@ class OrdersTab extends StatelessWidget {
         title: const Text('Pedidos'),
       ),
       body: ListView.separated(
+        padding: const EdgeInsets.all(16),
+        physics: const BouncingScrollPhysics(),
         separatorBuilder: (_, index) => const SizedBox(height: 10),
-        itemBuilder: (_, index) => OrderTile(
-          order: appData.orders[index],
-        ),
+        itemBuilder: (_, index) => OrderTile(order: appData.orders[index]),
         itemCount: appData.orders.length,
       ),
     );
