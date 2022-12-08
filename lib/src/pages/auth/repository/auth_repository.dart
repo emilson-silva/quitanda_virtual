d9d87bf832b1ac929ef132cddd82a7f1bff47a2b
Emilson Silva <emilsongyn17@gmail.com>
2022-12-08 02:01:39 -0300

    Criando o método de validação do token

--- lib/src/pages/auth/repository/auth_repository.dart
+++ lib/src/pages/auth/repository/auth_repository.dart
@@ -8,14 +8,33 @@ import 'package:quitanda_virtual/src/services/http_manager.dart';
 class AuthRepository {
   final HttpMManager _httpManger = HttpMManager();
 
+  Future<AuthResult> validateToken(String token) async {
+    final result = await _httpManger.restRequest(
+      url: Endpoints.validateToken,
+      method: HttpMethods.post,
+      headers: {
+        'X-Parse-Session-Token': token,
+      },
+    );
+
+    if (result['result'] != null) {
+      final user = UserModel.fromJson(result['result']);
+
+      return AuthResult.success(user);
+    } else {
+      return AuthResult.error(
+          authErrorsSTring.authErrorsSTring(result['error']));
+    }
+  }
+
   Future<AuthResult> signIn(
       {required String email, required String password}) async {
     final result = await _httpManger.restRequest(
       url: Endpoints.signin,
       method: HttpMethods.post,
       body: {
-        "email": email,
-        "password": password,
+        'email': email,
+        'password': password,
       },
     );
 
