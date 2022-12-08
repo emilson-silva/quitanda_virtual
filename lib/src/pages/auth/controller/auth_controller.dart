a677b3fc67740b80de10e3520c9cb10cb6ecc56a
Emilson Silva <emilsongyn17@gmail.com>
2022-12-08 02:35:47 -0300

    Salvando o token localmente

--- lib/src/pages/auth/controller/auth_controller.dart
+++ lib/src/pages/auth/controller/auth_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:quitanda_virtual/src/constants/storage_keys.dart';
 import 'package:quitanda_virtual/src/models/user_model.dart';
 import 'package:quitanda_virtual/src/pages/auth/repository/auth_repository.dart';
 import 'package:quitanda_virtual/src/pages/auth/result/auth_result.dart';
@@ -14,7 +15,43 @@ class AuthController extends GetxController {
   UserModel user = UserModel();
 
   Future<void> validateToken() async {
-    authRepository.validateToken(token);
+    //  Recuperar o token que foi salvo localmente
+    String? token = await utilsServices.getLocalData(key: StorageKeys.token);
+    if (token == null) {
+      Get.offAllNamed(PagesRoutes.signInRoute);
+      return;
+    }
+
+    AuthResult result = await authRepository.validateToken(token);
+
+    result.when(
+      success: (user) {
+        this.user = user;
+
+        saveTokenAndProceedToBase();
+      },
+      error: (message) {
+        signOut();
+      },
+    );
+  }
+
+  Future<void> signOut() async {
+    // Zerar o user
+    user = UserModel();
+
+    // Remover o token localmente
+    await utilsServices.removeLocalData(key: StorageKeys.token);
+
+    // Ir para o login
+    Get.offAllNamed(PagesRoutes.signInRoute);
+  }
+
+  void saveTokenAndProceedToBase() {
+// Salvar o token
+    utilsServices.saveLocalData(key: StorageKeys.token, data: user.token!);
+
+// Ir para a base
   }
 
   Future<void> signIn({
@@ -32,7 +69,7 @@ class AuthController extends GetxController {
       success: (user) {
         this.user = user;
 
-        Get.offAllNamed(PagesRoutes.baseRoute);
+        saveTokenAndProceedToBase();
       },
       error: (message) {
         utilsServices.showToast(
