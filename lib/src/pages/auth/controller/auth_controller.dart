bf3d6875bcc73098a13c527d9744393f044192df
Emilson Silva <emilsongyn17@gmail.com>
2022-12-08 01:37:43 -0300

    navegando para a tela base

--- lib/src/pages/auth/controller/auth_controller.dart
+++ lib/src/pages/auth/controller/auth_controller.dart
@@ -1,11 +1,17 @@
 import 'package:get/get.dart';
+import 'package:quitanda_virtual/src/models/user_model.dart';
 import 'package:quitanda_virtual/src/pages/auth/repository/auth_repository.dart';
 import 'package:quitanda_virtual/src/pages/auth/result/auth_result.dart';
+import 'package:quitanda_virtual/src/pages_routes/app_pages.dart';
+import 'package:quitanda_virtual/src/services/utils_services.dart';
 
 class AuthController extends GetxController {
   RxBool isLoading = false.obs;
 
   final authRepository = AuthRepository();
+  final utilsServices = UtilsServices();
+
+  UserModel user = UserModel();
 
   Future<void> signIn({
     required String email,
@@ -20,10 +26,15 @@ class AuthController extends GetxController {
 
     result.when(
       success: (user) {
-        print(user);
+        this.user = user;
+
+        Get.offAllNamed(PagesRoutes.baseRoute);
       },
       error: (message) {
-        print(message);
+        utilsServices.showToast(
+          message: message,
+          isError: true,
+        );
       },
     );
   }
