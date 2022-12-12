1f2f51073c332a4ce3ab8bd042c7ff5a76cccef5
Emilson Silva <emilsongyn17@gmail.com>
2022-12-12 09:20:05 -0300

    ajustes data pix

--- lib/src/services/utils_services.dart
+++ lib/src/services/utils_services.dart
@@ -36,7 +36,7 @@ class UtilsServices {
     initializeDateFormatting();
 
     DateFormat dateFormat = DateFormat.yMd('pt_BR').add_Hm();
-    return dateFormat.format(dateTime);
+    return dateFormat.format(dateTime.toLocal());
   }
 
   Uint8List decodeQrCodeImage(String value) {
